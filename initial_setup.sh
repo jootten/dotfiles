7afc77fdbe57b8280066c1166177189ca8cf3511
Jonas Otten <jootten@uos.de>
2022-05-04 12:18:57 +0000

    Minor adjustments

--- initial_setup.sh
+++ initial_setup.sh
@@ -2,9 +2,12 @@ gh auth login
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/jootten/dotfiles.git $HOME/mydotfiles
 rm -r $HOME/mydotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+config checkout -- .
+config checkout vlab-vm
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install.sh --keep-zshrc --unattended
 rm install.sh
+conda create -n benchmark python=3.8 virtualenv
 sudo chsh -s /bin/zsh $USER
-pip install pynvim
+/opt/conda/bin/python -m pip install pynvim
 sudo apt install ripgrep
